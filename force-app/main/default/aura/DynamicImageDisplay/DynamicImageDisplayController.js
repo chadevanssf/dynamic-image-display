dce60f01ead19b8c915c9ff23a4472a0ffd3710d
Chad Evans <chad.evans@salesforce.com>
2019-07-26 17:36:21 -0400

    Cleanup for Summer 19

--- force-app/main/default/aura/DynamicImageDisplay/DynamicImageDisplayController.js
+++ force-app/main/default/aura/DynamicImageDisplay/DynamicImageDisplayController.js
@@ -1,12 +1,70 @@
 ({
     doInit : function(component, event, helper) {
-        var fieldName = component.get("v.fieldName");
-
+        const fieldName = component.get("v.fieldName");
+        
         var fieldNames = [];
         fieldNames.push(fieldName);
-
+        
         component.set("v.fieldNames", fieldNames);
-
+        
+        const empApi = component.find("empApi");
+        if (empApi) {
+            // Uncomment below line to enable debug logging (optional)
+            empApi.setDebugFlag(true);
+            // Register error listener and pass in the error handler function
+            empApi.onError($A.getCallback(function(error) {
+                console.error('EMP API error: ', error);
+            }));
+            const channel = component.get("v.channel");
+            const replayId = -1;
+            empApi.subscribe(channel, replayId, $A.getCallback(function(eventReceived) {
+                var live = component.find("live");
+                
+                $A.util.removeClass(live, "slds-theme_warning");
+                $A.util.addClass(live, "slds-theme_success");
+                
+                var fieldName = component.get("v.payloadFieldName");
+                var payload = eventReceived.data.payload;
+                if (component.get("v.filterIdFieldName") !== "" &&
+                    component.get("v.filterPayloadFieldName") !== "") {
+                    var rec = component.get("v.simpleRecord");
+                    var filterValue = rec[component.get("v.filterIdFieldName")];
+                    if (payload[component.get("v.filterPayloadFieldName")] === filterValue) {
+                        component.set("v.currentStatus", payload[fieldName]);
+                        helper.updateStatus(component);
+                    }
+                } else {
+                    component.set("v.currentStatus", payload[fieldName]);
+                    helper.updateStatus(component);
+                }
+                
+                var delay = 1000;
+                window.setTimeout(
+                    $A.getCallback(function() {
+                        if (component.isValid()) {
+                            $A.util.addClass(live, "slds-theme_warning");
+                            $A.util.removeClass(live, "slds-theme_success");
+                        }}),
+                    delay);       
+            }))
+            .then(function(subscription) {
+                // Save subscription to unsubscribe later
+                component.set("v.subscription", subscription);
+                
+                var live = component.find("live");
+                var delay = 500;
+                window.setTimeout(
+                    $A.getCallback(function() {
+                        if (component.isValid()) {
+                            live.set("v.label", "live");
+                            $A.util.removeClass(live, "slds-theme_offline");
+                            $A.util.addClass(live, "slds-theme_warning");
+                            $A.util.removeClass(live, "slds-theme_success");
+                        }}),
+                    delay); 
+            });
+        }
+        
         var action = component.get("c.getConfig");
         action.setStorable();
         action.setParams(
@@ -24,23 +82,8 @@
                 } else {
                     var config = JSON.parse(configValue);
                     component.set("v.config", config);
-
-                    var dynamicComponentsByAuraId = { "config": "true" };
-                    component.set("v.dynamicComponentsByAuraId", dynamicComponentsByAuraId);
-
-                    config.forEach(function (item) {
-                        var layerName = item.layername;
-                        var imageLocation = item.image;
-                        var archiveLocation = item.archive;
-                        var combined = "layer_" + layerName;
-                        var classInfo = "slds-float_left layer " + combined;
-                        if (layerName === "base") {
-                            classInfo += " show";
-                        } else {
-                            classInfo += " hide";
-                        }
-                        helper.addImageElement(component, combined, classInfo, imageLocation, archiveLocation, helper.updateStatus);
-                    }); // forEach
+                    
+                    helper.addImages(component, config);
                 }
             }  else if (state === "ERROR") {
                 $A.log(resp.getError());
@@ -50,16 +93,16 @@
         });
         $A.enqueueAction(action);
     },
-
+    
     changeRecord : function(component, event, helper) {
         var rec = component.find("record");
         rec.reloadRecord();
     },
-
+    
     recordUpdated : function(component, event, helper) {
         // when the record is loaded for the first time, or updated in the interface
         var changeType = event.getParams().changeType;
-
+        
         if (changeType === "ERROR") {
             /* handle error; do this first! */
         } else if (changeType === "LOADED" || changeType === "CHANGED") {
@@ -75,44 +118,5 @@
         component.set("v.recordId", id);
         var rec = component.find("record");
         rec.reloadRecord();
-    },
-
-    handleStart : function(component, event, helper) {
-        var live = component.find("live");
-        live.set("v.label", "live");
-        $A.util.removeClass(live, "slds-theme_offline");
-        $A.util.addClass(live, "slds-theme_warning");
-        $A.util.removeClass(live, "slds-theme_success");
-    },
-
-    handleMessage : function(component, event, helper) {
-        var live = component.find("live");
-
-        $A.util.removeClass(live, "slds-theme_warning");
-        $A.util.addClass(live, "slds-theme_success");
-
-        var fieldName = component.get("v.payloadFieldName");
-        var payload = event.getParam("payload");
-        if (component.get("v.filterIdFieldName") !== "" &&
-            component.get("v.filterPayloadFieldName") !== "") {
-            var rec = component.get("v.simpleRecord");
-            var filterValue = rec[component.get("v.filterIdFieldName")];
-            if (payload[component.get("v.filterPayloadFieldName")] === filterValue) {
-                component.set("v.currentStatus", payload[fieldName]);
-                helper.updateStatus(component);
-            }
-        } else {
-            component.set("v.currentStatus", payload[fieldName]);
-            helper.updateStatus(component);
-        }
-
-        var delay = 1000;
-        window.setTimeout(
-            $A.getCallback(function() {
-                if (component.isValid()) {
-                    $A.util.addClass(live, "slds-theme_warning");
-                    $A.util.removeClass(live, "slds-theme_success");
-                }}),
-            delay);
     }
 })
